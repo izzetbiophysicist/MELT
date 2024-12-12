1f4863bfe406c1b3c87b28e86387b00e778de0cd
Lucas Machado <66788893+izzetbiophysicist@users.noreply.github.com>
2024-12-12 14:56:22 -0300

    Update Step1_NormalModes.R

--- Step1_NormalModes.R
+++ Step1_NormalModes.R
@@ -5,45 +5,77 @@ pdb <- read.pdb("2LZT")
 
 # Trim the PDB file to remove certain atoms:
 # Exclude HETATM entries and hydrogen atoms
-# The 'operator = "OR"' indicates a logical OR operation for selection
-# 'inverse = TRUE' means all atoms except those matching the criteria will be kept
 pdb <- trim.pdb(pdb, atom.select(pdb, type = 'HETATM', elesy = "H", 
                                  operator = "OR", inverse = TRUE))
 
 # Perform normal mode analysis (NMA) on the trimmed structure
-# 'outmodes = "noh"' indicates excluding hydrogen atoms from output modes
-# 'rm.wat = TRUE' removes water molecules from the analysis
 nma <- aanma(pdb, outmodes = 'noh', rm.wat = TRUE)
 
-# Function to apply displacement in a specified direction and mode
+# Function to apply displacement in a specified manner (Factor or Geometrical)
 # Args:
 #   pdb: The original PDB object
 #   nma: The NMA results containing the mode vectors
 #   mode: The mode number to use for displacement
 #   direction: The direction of displacement, either "plus" or "minus"
-#   scale: The scaling factor for the displacement (default is 10)
-#   output_file: The name of the output PDB file
-apply_displacement <- function(pdb, nma, mode, direction, scale = 10) {
-  # Copy the original pdb object to preserve it
-  pdb_modified <- pdb
-  
-  # Apply displacement based on the specified direction
-  if (direction == "minus") {
-    pdb_modified$xyz <- pdb_modified$xyz - scale * nma$modes[, mode]
-  } else if (direction == "plus") {
-    pdb_modified$xyz <- pdb_modified$xyz + scale * nma$modes[, mode]
+#   scale: The scaling factor for the Factor method (default is 10)
+#   rms: The RMS value for the Geometrical method
+#   selection: Atom selection for the Geometrical method ('calpha' or 'all')
+#   method: The method to use for displacement ('Factor' or 'Geometrical')
+apply_displacement <- function(pdb, nma, mode, direction, scale = 10, rms = 1.0, 
+                               selection = 'calpha', method = 'Factor') {
+  if (method == 'Factor') {
+    # Displacement using the Factor method
+    pdb_modified <- pdb
+
+    # Apply displacement based on the specified direction
+    if (direction == "minus") {
+      pdb_modified$xyz <- pdb_modified$xyz - scale * nma$modes[, mode]
+    } else if (direction == "plus") {
+      pdb_modified$xyz <- pdb_modified$xyz + scale * nma$modes[, mode]
+    } else {
+      stop("Invalid direction. Please specify 'plus' or 'minus'.")
+    }
+
+    # Generate output file name
+    output_file <- paste("desloc_", mode, "_", direction, "_factor.pdb", sep = "")
+
+    # Write the modified PDB structure to a file
+    write.pdb(pdb_modified, file = output_file)
+  } else if (method == 'Geometrical') {
+    # Displacement using the Geometrical method
+    xyz <- pdb$xyz
+
+    # Select atom indices based on input
+    if (selection == 'calpha') {
+      inds <- atom.select(pdb, elety = 'CA')$xyz
+    } else if (selection == 'all') {
+      inds <- atom.select(pdb, string = 'protein')$xyz
+    } else {
+      stop("Invalid selection. Choose 'calpha' or 'all'.")
+    }
+
+    # Compute scaling factor
+    vec <- nma$modes[, mode]
+    mag <- sqrt(length(inds)) * rms / sqrt(sum(vec^2))
+
+    # Apply displacement
+    displaced_xyz <- xyz
+    displaced_xyz[inds] <- xyz[inds] + vec[inds] * mag
+
+    # Generate output file name
+    output_file <- paste("desloc_", mode, "_", direction, "_geometrical.pdb", sep = "")
+
+    # Write to PDB
+    write.pdb(pdb = pdb, xyz = displaced_xyz, file = output_file)
   } else {
-    stop("Invalid direction. Please specify 'plus' or 'minus'.")
+    stop("Invalid method. Please specify 'Factor' or 'Geometrical'.")
   }
-  
-  # Write the modified PDB structure to a file
-  write.pdb(pdb_modified, file = paste("desloc_",mode,"_",direction,".pdb", sep = ""))
 }
 
-# Example usage: Displacing using mode 7 in the minus direction
+# Example usage: Displacing using mode 7 in the minus direction with Factor method
 apply_displacement(pdb, nma, mode = 7, direction = "minus", 
-                   scale = 10)
+                   scale = 10, method = 'Factor')
 
-# Example usage: Displacing using mode 7 in the plus direction
+# Example usage: Displacing using mode 7 in the plus direction with Geometrical method
 apply_displacement(pdb, nma, mode = 7, direction = "plus", 
-                   scale = 10)
+                   rms = 1.0, selection = 'calpha', method = 'Geometrical')
